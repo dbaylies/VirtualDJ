af8e8022ff445515c9a3cd5f4c82c065bcd9dd71
David Baylies <dbaylies@gmail.com>
2018-07-11 13:48:00 -0400

    Added multichannel input, streaming to two AudioSources

---
+++ Assets/StreamAudio1.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Uses the Microphone class to read audio into an AudioSource's clip from an external audio source, while simultaneously playing back the
+/// audio clip. This takes input from two external sources and plays them back through two AudioSources.
+/// To have 3D spatial blend, you MUST start the scene with completely 2D spatial blend, then change the slider to 3D AFTER the scene has started.
+/// Not sure why this is, it's likely a bug.
+/// 
+/// There are also occasionally audio glitches, as well as a persitent reverberation whose sources I cannot locate.
+/// 
+/// -David B.
+/// </summary>
+
+
+public class StreamAudio1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        // Check the console to see which devices Unity is recognizing
+        foreach (string device in Microphone.devices)
+        {
+            Debug.Log("Name: " + device);
+        }
+        
+
+        var audio = GetComponent<AudioSource>();
+        // Change the index of Microphone.devices[0] to choose the input device for this audio source
+        audio.clip = Microphone.Start(deviceName: Microphone.devices[0], loop: true, lengthSec: 5, frequency: 44100);
+        audio.loop = true;
+        // Change it here too
+        while (!(Microphone.GetPosition(Microphone.devices[0]) > 0)) { }
+        audio.Play();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/StreamAudio2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StreamAudio2 : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        // See StreamAudio1.cs for some useful comments
+        var audio = GetComponent<AudioSource>();
+        audio.clip = Microphone.Start(deviceName: Microphone.devices[1], loop: true, lengthSec: 5, frequency: 44100);
+        audio.loop = true;
+        while (!(Microphone.GetPosition(Microphone.devices[1]) > 0)) { }
+        audio.Play();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
